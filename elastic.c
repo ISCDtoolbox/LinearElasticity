49b563be80456e560303ae027e1d37aabdd9f168
Pascal FREY <frey@macbookpro-de-pascal.home>
2015-09-03 04:07:37 +0200

    minor typo

--- elastic.c
+++ elastic.c
@@ -15,7 +15,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Sulastic.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SUlastic.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "elastic.h"
