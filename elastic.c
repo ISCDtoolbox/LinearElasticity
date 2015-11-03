5acefc279edbf2cc076cd9ac4a4156dc40cbfda7
Pascal FREY <frey@macbookpro-de-pascal.home>
2015-11-03 22:38:13 +0100

    fixed minor typos

--- elastic.c
+++ elastic.c
@@ -4,12 +4,12 @@
  *
  * This file is part of SUlastic.
  *
- * Sulastic is free software: you can redistribute it and/or modify it under
+ * SUlastic is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Sulastic is distributed in the hope that it will be useful,
+ * SUlastic is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
